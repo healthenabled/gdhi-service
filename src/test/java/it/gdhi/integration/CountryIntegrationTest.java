90b88aa634205a8371590becbb3c116fc7ae4f6d
Pavithra <pavithra@thoughtworks.com>
2017-11-27 08:43:56 +0530

    GDHI-TECH Integration test framework  <Pavithra>

--- src/test/java/it/gdhi/integration/CountryIntegrationTest.java
+++ src/test/java/it/gdhi/integration/CountryIntegrationTest.java
@@ -2,14 +2,21 @@ package it.gdhi.integration;
 
 import io.restassured.response.Response;
 import it.gdhi.Application;
+import it.gdhi.model.HealthIndicator;
+import it.gdhi.model.id.HealthIndicatorId;
 import it.gdhi.repository.IHealthIndicatorRepository;
+import org.junit.After;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.jdbc.JdbcTestUtils;
+
+import javax.sql.DataSource;
 
 import static io.restassured.RestAssured.given;
 
@@ -22,44 +29,46 @@ public class CountryIntegrationTest {
 
     @Autowired
     private IHealthIndicatorRepository healthIndicatorRepository;
+    @Autowired
+    private DataSource dataSource;
 
     @Test
     public void shouldGetHealthIndicatorForACountry() throws Exception {
-//        String countryId = "IND";
-//        Integer categoryId1 = 1;
-//        Integer categoryId2 = 2;
-//        Integer categoryId3 = 3;
-//        Integer indicatorId1_1 = 1;
-//        Integer indicatorId1_2 = 2;
-//        Integer indicatorId2_1 = 3;
-//        Integer indicatorId2_2 = 4;
-//        Integer indicatorId3_1 = 5;
-//        Integer indicatorId3_2 = 6;
-//
-//
-//        HealthIndicatorId healthIndicatorId1 = new HealthIndicatorId(countryId,categoryId1,indicatorId1_1);
-//        HealthIndicator healthIndicatorSetupData1 = new HealthIndicator(healthIndicatorId1, 1, "sp1");
-//        healthIndicatorRepository.save(healthIndicatorSetupData1);
-//
-//        HealthIndicatorId healthIndicatorId2 = new HealthIndicatorId(countryId,categoryId1,indicatorId1_2);
-//        HealthIndicator healthIndicatorSetupData2 = new HealthIndicator(healthIndicatorId2, 2, "sp1");
-//        healthIndicatorRepository.save(healthIndicatorSetupData2);
-//
-//        HealthIndicatorId healthIndicatorId3 = new HealthIndicatorId(countryId,categoryId2,indicatorId2_1);
-//        HealthIndicator healthIndicatorSetupData3 = new HealthIndicator(healthIndicatorId3, 3, "sp1");
-//        healthIndicatorRepository.save(healthIndicatorSetupData3);
-//
-//        HealthIndicatorId healthIndicatorId4 = new HealthIndicatorId(countryId,categoryId2, indicatorId2_2);
-//        HealthIndicator healthIndicatorSetupData4 = new HealthIndicator(healthIndicatorId4, null, "sp1");
-//        healthIndicatorRepository.save(healthIndicatorSetupData4);
-//
-//        HealthIndicatorId healthIndicatorId5 = new HealthIndicatorId(countryId,categoryId3,indicatorId3_1);
-//        HealthIndicator healthIndicatorSetupData5 = new HealthIndicator(healthIndicatorId5, null, "sp1");
-//        healthIndicatorRepository.save(healthIndicatorSetupData5);
-//
-//        HealthIndicatorId healthIndicatorId6 = new HealthIndicatorId(countryId,categoryId3,indicatorId3_2);
-//        HealthIndicator healthIndicatorSetupData6 = new HealthIndicator(healthIndicatorId6, null, "sp1");
-//        healthIndicatorRepository.save(healthIndicatorSetupData6);
+        String countryId = "IND";
+        Integer categoryId1 = 1;
+        Integer categoryId2 = 2;
+        Integer categoryId3 = 3;
+        Integer indicatorId1_1 = 1;
+        Integer indicatorId1_2 = 2;
+        Integer indicatorId2_1 = 3;
+        Integer indicatorId2_2 = 4;
+        Integer indicatorId3_1 = 5;
+        Integer indicatorId3_2 = 6;
+
+
+        HealthIndicatorId healthIndicatorId1 = new HealthIndicatorId(countryId,categoryId1,indicatorId1_1);
+        HealthIndicator healthIndicatorSetupData1 = new HealthIndicator(healthIndicatorId1, 1, "sp1");
+        healthIndicatorRepository.save(healthIndicatorSetupData1);
+
+        HealthIndicatorId healthIndicatorId2 = new HealthIndicatorId(countryId,categoryId1,indicatorId1_2);
+        HealthIndicator healthIndicatorSetupData2 = new HealthIndicator(healthIndicatorId2, 2, "sp1");
+        healthIndicatorRepository.save(healthIndicatorSetupData2);
+
+        HealthIndicatorId healthIndicatorId3 = new HealthIndicatorId(countryId,categoryId2,indicatorId2_1);
+        HealthIndicator healthIndicatorSetupData3 = new HealthIndicator(healthIndicatorId3, 3, "sp1");
+        healthIndicatorRepository.save(healthIndicatorSetupData3);
+
+        HealthIndicatorId healthIndicatorId4 = new HealthIndicatorId(countryId,categoryId2, indicatorId2_2);
+        HealthIndicator healthIndicatorSetupData4 = new HealthIndicator(healthIndicatorId4, null, "sp1");
+        healthIndicatorRepository.save(healthIndicatorSetupData4);
+
+        HealthIndicatorId healthIndicatorId5 = new HealthIndicatorId(countryId,categoryId3,indicatorId3_1);
+        HealthIndicator healthIndicatorSetupData5 = new HealthIndicator(healthIndicatorId5, null, "sp1");
+        healthIndicatorRepository.save(healthIndicatorSetupData5);
+
+        HealthIndicatorId healthIndicatorId6 = new HealthIndicatorId(countryId,categoryId3,indicatorId3_2);
+        HealthIndicator healthIndicatorSetupData6 = new HealthIndicator(healthIndicatorId6, null, "sp1");
+        healthIndicatorRepository.save(healthIndicatorSetupData6);
 
 
         Response response = given()
@@ -68,5 +77,11 @@ public class CountryIntegrationTest {
                 .get("http://localhost:" + port + "/countries/IND/health_indicators");
 
         response.prettyPrint();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+        JdbcTestUtils.deleteFromTables(jdbcTemplate,"validated_config.health_indicators");
     }
 }
