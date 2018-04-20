4b868367385e92576e1e442f26c0c18b97f8297b
Pugazh <pugal.sym@gmail.com>
2018-04-20 14:42:41 +0530

    GDHI-52 Adding /admin for the review URL in Email <Pugazh/Ginette>

--- src/main/java/it/gdhi/utils/Constants.java
+++ src/main/java/it/gdhi/utils/Constants.java
@@ -5,7 +5,7 @@ public class Constants {
 
     public static class Mail {
         public static final String SUBJECT = "GDHI has a new response from %s";
-        public static final String HEALTH_INDICATOR_PATH = "%s/health_indicator_questionnaire/%s/review";
+        public static final String HEALTH_INDICATOR_PATH = "%s/admin/health_indicator_questionnaire/%s/review";
         public static final String BODY = "Hello %s  ,\n" +
                 "%s, %s has provided response for %s, in the GDHI website.\n" +
                 "Contact: %s \n" +
